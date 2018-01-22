1ecd019dd9eafdeb6405a329099330f74e2ab89a
Koolant <koolan413@gmail.com>
2018-01-21 19:30:35 -0800

    State Machine initial commit

---
+++ Tactics/Assets/Scripts/Demo/NewBehaviourScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnEnable()
+    {
+        InputController.moveEvent += OnMoveEvent;
+        InputController.fireEvent += OnFireEvent;
+    }
+
+    private void OnMoveEvent(object sender, InfoEventArgs<Point> e)
+    {
+        Debug.Log("Move " + e.info.ToString());
+    }
+
+    private void OnDisable()
+    {
+        InputController.moveEvent -= OnMoveEvent;
+        InputController.fireEvent -= OnFireEvent;
+    }
+
+    private void OnFireEvent(object sender, InfoEventArgs<int> e)
+    {
+        Debug.Log("Fire " + e.info.ToString());
+    }
+}
